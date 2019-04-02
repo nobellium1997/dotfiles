687edebcc56fc80ded14f1d28fd80c1b3f708202
Nobel <nobarakat@gmail.com>
2019-04-02 02:27:24 -0600

    updates to rc and configs

--- update_repo.sh
+++ update_repo.sh
@@ -1,6 +1,6 @@
 #!/bin/sh 
 cp ~/.bashrc /mnt/c/Users/nobel/i3-dotfiles/rc_files/bashrc
-cp ~/.inpturc /mnt/c/Users/nobel/i3-dotfiles/rc_files/inputrc
+cp ~/.inputrc /mnt/c/Users/nobel/i3-dotfiles/rc_files/inputrc
 cp ~/.tmux.conf /mnt/c/Users/nobel/i3-dotfiles/tmux_conf
 cd /mnt/c/Users/nobel/i3-dotfiles
 git commit -am "updates to rc and configs"

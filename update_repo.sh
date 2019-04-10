221204f1203a85d3aac76ab9b8c702720e3018dd
Nobel <nobarakat@gmail.com>
2019-04-09 22:22:04 -0600

    updates to rc and configs

--- update_repo.sh
+++ update_repo.sh
@@ -2,5 +2,7 @@
 cp ~/.bashrc rc_files/bashrc
 cp ~/.inputrc rc_files/inputrc
 cp ~/.tmux.conf configs/tmux_conf
+cp ~/.vimrc vim_configs/my_vimrc
+cp ~/.config/nvim/init.vim vim_configs/init.vim
 git commit -am "updates to rc and configs"
 git push
